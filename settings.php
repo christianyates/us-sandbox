b731269c665ab26c8fb7712addd6a49c06202444
chrisyates <chris@christianyates.com>
2013-07-27 11:00:00 -0400

    Added memcache module

--- settings.php
+++ settings.php
@@ -3,3 +3,7 @@
 if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/ussandbox/ussandbox-settings.inc');
 }
+$conf['cache_backends'][] = 'sites/default/modules/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+// The 'cache_form' bin must be assigned no non-volatile storage.
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
